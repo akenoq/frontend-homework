4e10a918fecc86e747d513706c96181056d3780c
akenoq <akenoq@yandex.ru>
2017-08-30 18:37:08 +0300

    var4fixfix Kuklina

--- source/inverse.js
+++ source/inverse.js
@@ -1,21 +1,16 @@
 // Куклина Нина 
-// Вариант 4 // fix
+// Вариант 4 // fixfix
 
 "use strict";
 
 function inverse(arr = [], n = 0) {
 
-    if (!n) {
-        return arr.reverse();
-    } else {
         let arrBuf = arr.slice();	
-        if (n > 0) {
-	    let arrReversePart = arr.splice(n,arr.length-n);
+        if (n >= 0) {
+	    let arrReversePart = arr.splice(n, arr.length - n);
             return arr.concat(arrReversePart.reverse());
-        } else {
-	    let arrReversePart = arr.splice(arr.length+n,-n);
-            return arr.reverse().concat(arrReversePart);
-        }
-    }
+        } 
 
+	let arrReversePart = arr.splice(arr.length + n, -n);
+        return arr.reverse().concat(arrReversePart);
 }
